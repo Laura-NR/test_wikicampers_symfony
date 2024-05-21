3ce7370fdf6ef44630abc4308a507e4acafea581
GENYST\laura <nunez.laura.e@protonmail.com>
2024-05-21 21:51:02 +0200

    Création des entités

---
+++ migrations/Version20240521194708.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240521194708 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE availability (id INT AUTO_INCREMENT NOT NULL, vehicle_id INT NOT NULL, depart_date DATE NOT NULL, return_date DATE NOT NULL, price_per_day NUMERIC(10, 2) NOT NULL, status TINYINT(1) NOT NULL, INDEX IDX_3FB7A2BF545317D1 (vehicle_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE availability ADD CONSTRAINT FK_3FB7A2BF545317D1 FOREIGN KEY (vehicle_id) REFERENCES vehicle (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE availability DROP FOREIGN KEY FK_3FB7A2BF545317D1');
+        $this->addSql('DROP TABLE availability');
+    }
+}
---
+++ src/Entity/Availability.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvailabilityRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvailabilityRepository::class)]
+class Availability
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Vehicle $vehicle = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $depart_date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $return_date = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price_per_day = null;
+
+    #[ORM\Column]
+    private ?bool $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getVehicle(): ?Vehicle
+    {
+        return $this->vehicle;
+    }
+
+    public function setVehicle(?Vehicle $vehicle): static
+    {
+        $this->vehicle = $vehicle;
+
+        return $this;
+    }
+
+    public function getDepartDate(): ?\DateTimeInterface
+    {
+        return $this->depart_date;
+    }
+
+    public function setDepartDate(\DateTimeInterface $depart_date): static
+    {
+        $this->depart_date = $depart_date;
+
+        return $this;
+    }
+
+    public function getReturnDate(): ?\DateTimeInterface
+    {
+        return $this->return_date;
+    }
+
+    public function setReturnDate(\DateTimeInterface $return_date): static
+    {
+        $this->return_date = $return_date;
+
+        return $this;
+    }
+
+    public function getPricePerDay(): ?string
+    {
+        return $this->price_per_day;
+    }
+
+    public function setPricePerDay(string $price_per_day): static
+    {
+        $this->price_per_day = $price_per_day;
+
+        return $this;
+    }
+
+    public function isStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(bool $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
