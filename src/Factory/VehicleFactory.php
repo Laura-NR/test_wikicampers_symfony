f8af71722cc725a4766f1e2f76eaa6001744f5df
GENYST\laura <nunez.laura.e@protonmail.com>
2024-05-29 00:52:15 +0200

    Mis à jour du Factory pour véhicule

--- src/Factory/VehicleFactory.php
+++ src/Factory/VehicleFactory.php
@@ -18,19 +18,23 @@ use Zenstruck\Foundry\Instantiator;
  * @method static Vehicle|Proxy last(string $sortedField = 'id')
  * @method static Vehicle|Proxy random(array $attributes = [])
  * @method static Vehicle|Proxy randomOrCreate(array $attributes = [])
- * @method static Vehicle[]|Proxy[] all()
- * @method static Vehicle[]|Proxy[] findBy(array $attributes)
- * @method static Vehicle[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static Vehicle[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
  * @method Vehicle|Proxy create(array|callable $attributes = [])
  */
 final class VehicleFactory extends ModelFactory
 {
+    private const CAR_BRANDS = [
+        'Toyota', 'Ford', 'Honda', 'Chevrolet', 'Nissan', 'BMW', 'Mercedes-Benz', 'Volkswagen', 'Audi', 'Hyundai'
+    ];
+
+    private const CAR_MODELS = [
+        'Camry', 'F-150', 'Civic', 'Silverado', 'Altima', '3 Series', 'C-Class', 'Golf', 'A4', 'Elantra'
+    ];
+
     protected function getDefaults(): array
     {
         return [
-            'make' => self::faker()->company,
-            'model' => self::faker()->word,
+            'make' => self::faker()->randomElement(self::CAR_BRANDS),
+            'model' => self::faker()->randomElement(self::CAR_MODELS),
         ];
     }
 
